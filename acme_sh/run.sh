6a64f774ae52cc2370174a80cd9ee4b71bbc7301
highTower.SU <3651850+highTowerSU@users.noreply.github.com>
2024-12-01 00:17:45 +0100

    Update run.sh

--- acme_sh/run.sh
+++ acme_sh/run.sh
@@ -6,7 +6,9 @@ DOMAINS=$(bashio::config 'domains')
 KEYFILE=$(bashio::config 'keyfile')
 CERTFILE=$(bashio::config 'certfile')
 DNS_PROVIDER=$(bashio::config 'dns.provider')
+DNS_SLEEP=$(bashio::config 'dns.sleep')
 DNS_ENVS=$(bashio::config 'dns.env')
+DOMAIN_ALIAS=$(bashio::config 'domainalias')
 
 for env in $DNS_ENVS; do
     export $env
@@ -22,11 +24,22 @@ if [ -n "$SERVER" ]; then
     SERVER_ARG="--server $SERVER"
 fi
 
+DOMAIN_ALIAS_ARG=""
+if [ -n "$DOMAIN_ALIAS" ]; then
+    DOMAIN_ALIAS_ARG="--domain-alias $DOMAIN_ALIAS"
+fi
+
+if [ -z $DNS_SLEEP ]; then
+    DNS_SLEEP=60
+fi
+
 /root/.acme.sh/acme.sh --register-account -m ${ACCOUNT} $SERVER_ARG
 
 /root/.acme.sh/acme.sh --issue "${DOMAIN_ARR[@]}" \
 --dns "$DNS_PROVIDER" \
-$SERVER_ARG
+--dnssleep $DNS_SLEEP \
+$SERVER_ARG \
+$DOMAIN_ALIAS_ARG
 
 /root/.acme.sh/acme.sh --install-cert "${DOMAIN_ARR[@]}" \
 --fullchain-file "/ssl/${CERTFILE}" \
